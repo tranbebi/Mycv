bbbd7b6f36d041d7e2cea5f0d802e51f1f11263c
tranbebi <bit92785@gmail.com>
2024-10-30 20:28:03 +0700

    lưu

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,19 @@
+"use client"; // Đánh dấu đây là Client Component
+
+import Spline from "@splinetool/react-spline";
 import Image from "next/image";
+import React from "react";
 
 export default function Home() {
   return (
     <>
-    <h1>trần bé bi nè</h1>
+     <Image
+        src="/z5979838940588_293905f8d2830a5a671de494bdb1b80e.jpg" // Đường dẫn tới ảnh
+        alt="Mô tả ảnh"           // Mô tả ảnh cho SEO và khả năng truy cập
+        width={1000}               // Chiều rộng của ảnh
+        height={300}              // Chiều cao của ảnh
+        quality={75}              // Chất lượng ảnh (tùy chọn)
+      />
     </>
   );
 }
